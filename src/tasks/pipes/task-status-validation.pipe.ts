5144caba54e480e029fcd67fb996e36d0e3fb805
Kebsi1993 <firas@littlebigsmala.com>
2020-03-31 09:56:46 +0100

    pipe validation

---
+++ src/tasks/pipes/task-status-validation.pipe.ts
@@ -0,0 +1,20 @@
+import { TaskStatus } from '../task.model';
+
+export class TaskStatusValidationPipe implements PipeTransform {
+  readonly allowedStatuses = [
+    TaskStatus.DONE,
+    TaskStatus.OPEN,
+    TaskStatus.IN_PROGRESS,
+  ];
+  transform(value: any) {
+    value = value.toUpperCase();
+    if (!this.isStatusValid(value)) {
+      throw new BadRequestException(`"${value} is an invalid status`);
+    }
+    return value;
+  }
+  private isStatusValid(status: any) {
+    const idx = this.allowedStatuses.indexOf(status);
+    return idx !== -1;
+  }
+}
